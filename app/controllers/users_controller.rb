dceecf19b936b3da5950abf4fca06e82a17aafe9
tommy-pellerin <tommytp@hotmail.fr>
2024-02-20 18:23:09 +0100

    page profil créé avec les events admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @user_events = current_user.events
+    @user_events = Event.all.where(admin:current_user.id)
+
   end
 end
